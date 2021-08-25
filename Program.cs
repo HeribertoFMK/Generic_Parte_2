32e878cbb600342a78117c4a354b9404881ac3e3
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-25 11:33:41 +0900

    Iniciando estudo sobre restrição de generics

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
